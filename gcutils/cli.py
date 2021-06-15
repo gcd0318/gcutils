af03e5d3597eb7917a409345af380ad555593a5a
gcd0318 <gcd0318@hotmail.com>
2021-06-15 18:30:24 +0000

    cli.py for cmd functions

--- gcutils/cli.py
+++ gcutils/cli.py
@@ -1,12 +1,83 @@
-import paramiko
-import socket
-import subprocess
-import time
-import traceback
+def run_shell_cmd(cmd):
+    try:
+        rc = subprocess.call(cmd, shell=True)
+        if rc != 0:
+            raise Exception(__name__, "Fail to run %s , rc: %s" % (cmd, rc))
+    except OSError as err:
+        raise Exception(__name__, err)
+    return rc
 
-from misc import exec_local_cmd
 
-from const import TIMEOUT_s, SHORT_s, SCRIPT_EXECUTE_TIMEOUT_s, RETRY, LOCALS
+def exec_local_cmd(args, shell=True, with_blank=False):
+    resl = []
+    try:
+        process = subprocess.Popen(args, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        rtcode = process.returncode
+        output = stdout + stderr
+        process.poll()
+        if (0 != rtcode):
+            output = stderr
+    except OSError as e:
+        rtcode = e.errno
+        output = "Fail to run command: %s" % e
+    for l in output.decode('utf-8').split('\n'):
+        if ((0 < len(l)) or with_blank):
+            resl.append(l)
+    return rtcode, resl
+
+
+def run_shell_script(args, shell=False, timeout=SCRIPT_EXECUTE_TIMEOUT_s, return_output=False):
+    command = Command(args)
+    return command.run(shell=shell, timeout=timeout, return_output=return_output)
+
+
+class Command(object):
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.process = None
+        self.error_message = None
+
+    def run(self, shell=False, timeout=SCRIPT_EXECUTE_TIMEOUT_s, return_output=False):
+        try:
+            def kill_process():
+                if self.process.poll() is None:
+                    self.process.kill()
+                    self.error_message = "shell process killed by timeout, timeout=%s" % timeout
+
+            self.process = subprocess.Popen(self.cmd, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                            close_fds=True)
+            # start timer
+            t = threading.Timer(timeout, kill_process)
+            t.start()
+
+            # wrap self.process.stdout with a NonBlockingStreamReader object:
+            nbsr = NonBlockingStreamReader(self.process.stdout)
+
+            shell_output = ""
+
+            # print the output in real-time
+            while self.process.poll() is None:
+                line = nbsr.readline(0.1)  # 0.1 secs to let the shell output the result
+                if line:
+                    shell_output += line
+
+            # When the subprocess terminates there might be unconsumed output that still needs to be processed.
+            content = nbsr.readline(1)
+            if content:
+                shell_output += content
+
+            # cancel timer
+            t.cancel()
+            if return_output:
+                output = shell_output if self.process.returncode == 0 else self.error_message
+                return self.process.returncode, output
+            else:
+                return self.process.returncode, self.error_message
+        except OSError as e:
+            error_message = "Fail to run command: %s" % e
+            return e.errno, error_message
+
 
 
 def remote_exec(cmd, ip, username, passkey=None, pkey=None, port=22, no_err=True, timeout=TIMEOUT_s, short_wait=SHORT_s, retry=RETRY, omit_str=None, platform='linux'):
@@ -83,22 +154,3 @@ def exec_cmd(cmd, machine='localhost', username=None, passkey=None, port=22, no_
         print(resl)
     return rtcode, resl
 
-
-
-def get_local_ip(port=80):
-    ip = None
-    i = 0
-    while (ip is None) and (i < len(DNS)):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            s.connect((DNS[i], port))
-            ip = s.getsockname()[0]
-        except Exception as err:  
-            raise Exception(__name__, err)
-        finally:
-            i = i + 1
-            s.close()
-    return ip
-
-def get_local_hostname():
-    return socket.gethostname() or None
