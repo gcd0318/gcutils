02cf09fcb51be9e05e9be5a18ab2c02d1ce9a342
gcd0318 <gcd0318@hotmail.com>
2025-02-03 19:13:56 +0800

    v 0.0.64

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setup(
         name = 'gcutils',
-        version = '0.0.63',
+        version = '0.0.64',
         author = 'gcd0318',
         author_email = 'gcd0318@hotmail.com',
         url = 'https://github.com/gcd0318/gcutils',
